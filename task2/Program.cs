606f53a4f219f6836d8a19440ae38cfdecf0c2cd
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-02-19 14:28:24 +0300

    выполненная вторая задача домашнего задания

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,19 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+System.Console.WriteLine("Введите чило: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = number1.ToString().Length;
+if (number2 >= 3)
+{
+  while (number1 > 999)
+  {
+    number1 = number1 / 10;
+  }
+  int result = number1 % 10;
+  Console.WriteLine("Третья Цифра " + result);
+}
+else
+{
+  Console.WriteLine("Третьей цифры нет");
+}
