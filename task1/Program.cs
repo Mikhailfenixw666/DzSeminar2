18c1415cf97865ec31120490ca452d8cb9dc6f0d
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-02-19 14:35:25 +0300

    первая задача домашнего задания

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,4 +2,12 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-
+System.Console.WriteLine("Введите трех значное число: ");
+int right = Convert.ToInt32(Console.ReadLine());
+int left = right.ToString().Length;
+if (left == 3) {
+	int result = ( right/10 ) % 10;
+	Console.WriteLine("Вторая цифра " + result);
+} else {
+	Console.WriteLine("Ошибка!!! Введите трехзначное число!!!");
+}
